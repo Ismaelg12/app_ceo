bd4657ef9ee3908b5f2916f00cda11c6811e04bc
Ismael <is_contabil@outlook.com>
2021-04-21 17:42:20 -0300

    correção de data

--- core/models.py
+++ core/models.py
@@ -46,7 +46,7 @@ class ListaEspera(models.Model):
     urgente           = models.CharField('Urgente', max_length=1, choices=URGENTE, blank=True)
 
     def get_idade(self):
-        return int((datetime.now().date()-self.criado_em).days)
+        return int((datetime.now().date()-self.criado_em).days/1)
 
     class Meta:
         verbose_name = 'Lista de Espera'
