18049f872230486cc6ba4887d6597091297e42e5
Ismael <is_contabil@outlook.com>
2021-05-07 21:37:56 -0300

    telefone base de dados

--- projeto/settings.py
+++ projeto/settings.py
@@ -89,8 +89,9 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databas
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -114,7 +115,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
