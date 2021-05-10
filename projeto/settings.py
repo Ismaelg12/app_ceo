753145ea81b5d358b87818f4bb26221086f543fb
Ismael <is_contabil@outlook.com>
2021-05-10 13:06:21 -0300

    Sistema emm produção correções feitas correção de banco

--- projeto/settings.py
+++ projeto/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+"""
 
 DATABASES = {
     'default': {
@@ -114,7 +114,7 @@ DATABASES = {
         },
     }
 }
-"""
+
 
 
 # Password validation
