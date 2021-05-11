1da731c3621fff6efb88c7758c791cbe5b7e54c0
Ismael <is_contabil@outlook.com>
2021-05-10 23:41:52 -0300

    projeto execultando com base correta

--- projeto/settings.py
+++ projeto/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -114,7 +114,7 @@ DATABASES = {
         },
     }
 }
-"""
+
 
 
 # Password validation
